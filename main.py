c03b7877918840f93de861e635218e7eb273d233
Ivan Orlando Ancallay Rea <ivan.ancallay@gmail.com>
2025-10-24 23:50:38 -0500

    Update: main.py

---
+++ main.py
@@ -0,0 +1,89 @@
+# TODO libreria os para compatibilidad de rutas entre sistemas operativos
+import os
+
+# TODO libreria sys para manejo de rutas de importacion de modulos
+import sys
+import pandas as pd
+
+from scripts.data_loader import DATA_PATH, cargar_datos_excel
+from scripts.superstore_clean import (
+    convertir_a_mayusculas,
+    ordenar_y_formatear_fecha,
+    renombrar_columnas,
+    verificar_nulos
+)
+
+from scripts.superstore_saving import guardar_datos_limpios
+from scripts.superstore_preparation import preparar_datos_para_analisis
+from scripts.superstore_groupin import agrupar_ventas
+# -----------------------------------------------------------
+# Importar módulos del proyecto
+# -----------------------------------------------------------
+# Agregar la carpeta "scripts" al path para poder importar desde allí
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'scripts'))
+
+# Importar tus módulos
+
+# -----------------------------------------------------------
+# Configuración de rutas
+# -----------------------------------------------------------
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_PATH = os.path.join(BASE_DIR, "data", "processed", "superstore_clean.csv")
+columnas_actuales = {
+    "Row ID": "Row_Id",
+    "Order ID": "Order_Id",
+    "Order Date": "Order_Date",
+    "Ship Date": "Ship_Date",
+    "Ship Mode": "Ship_Mode",
+    "Customer ID": "Customer_Id",
+    "Customer Name": "Customer_Name",
+    "Segment": "Segment",
+    "Country": "Country",
+    "City": "City",
+    "State": "State",
+    "Postal Code": "Postal_Code",
+    "Region": "Region",
+    "Product ID": "Product_Id",
+    "Category": "Category",
+    "Sub-Category": "Sub_Category",
+    "Product Name": "Product_Name",
+    "Sales": "Sales",
+    "Quantity": "Quantity",
+    "Discount": "Discount",
+    "Profit": "Profit"
+}
+
+if __name__ == "__main__":
+    # Cargar los datos
+    print("Cargando datos {DATA_PATH} ...")
+    df_superstore = cargar_datos_excel(DATA_PATH)
+
+    # Verificar valores nulos
+    resumen_nulos = verificar_nulos(df_superstore)
+    print(resumen_nulos)
+    
+    # Renombrar columnas
+    print("Renombrando columnas para evitar espacios ...")
+    df_superstore_renamed = renombrar_columnas(df_superstore, columnas_actuales)
+    # print(df_superstore.columns)
+    
+    # Convertir a mayúsculas el texto de todas las columnas de tipo texto
+    df_superstore_upper = convertir_a_mayusculas(df_superstore_renamed)
+    
+    # Ordenar y formatear la columna de fecha 'Order_Date'
+    df_superstore_order_date = ordenar_y_formatear_fecha(df_superstore_upper, 'Order_Date')
+    
+    # Ordenar y formatear la columna de fecha 'Ship_Date'
+    df_superstore_ship_date = ordenar_y_formatear_fecha(df_superstore_order_date, 'Ship_Date')
+    
+    print(df_superstore_ship_date.head(5))
+    
+    df_final = preparar_datos_para_analisis(df_superstore_ship_date)
+    print(df_final.columns)
+    
+    df_grouped = agrupar_ventas(df_final, nivel="mensual", por=["Region", "Category"])
+    print(df_grouped.head(5))
+    
+    # Guardar los datos limpios
+    guardar_datos_limpios(df_final, OUTPUT_PATH)
--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,4 +1,3 @@
-#%%
 import pandas as pd
 # TODO For file path operations without needing to import additional libraries in different environments Windows, Linus ux, MacOS
 import os 
@@ -7,10 +6,10 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # TODO ruta absoluta de la carpeta data (../data)
-DATA_PATH = os.path.join(SCRIPT_DIR, '..', 'data', "US_0Superstore_data.xls")
+DATA_PATH = os.path.join(SCRIPT_DIR, '..', 'data', "US_Superstore_data.xls")
 
 # TODO crear la funcion load_data
-def cargar_datos(path):
+def cargar_datos_excel(path):
     print(f"Cargando datos desde {path}...")
     
     try:
@@ -19,19 +18,8 @@ def cargar_datos(path):
         return df
     except FileNotFoundError:
         print(f"Error: El archivo en la ruta {path} no fue encontrado.")
-        print("Asegurate de tener el archivo en la carpeta 'data'.")
-        print("También puedes verificar que la ruta sea la correcta.")  
         return None
     except Exception as e:
         print(f"Error al cargar los datos: {e}")
         return None
 
-# TODO Este archivo se esta ejecutando directamente por el usuario o esta siendo importado por otro script?
-if __name__ == "__main__":        
-    print(f"Ejecutando el script desde: {SCRIPT_DIR}")
-    dataframe_matriz = cargar_datos(DATA_PATH)
-    if dataframe_matriz is not None:
-        print("\nPrimeras filas del DataFrame cargado:")
-        print(dataframe_matriz.head())
-        print("\nInformación del DataFrame:")
-        dataframe_matriz.info()
